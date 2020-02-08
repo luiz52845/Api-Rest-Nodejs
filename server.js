ca78383932e4c48f9810d42ba13009e4f5c3c278
data52845 <luizes_@hotmail.com>
2020-02-07 23:53:51 -0300

    trocando as portas

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+var porta = process.env.PORT || 8080;
+
+
 app.get("/", (req, res)=>{
   res.send('Olá Mundo')
 });
 
 
 
-app.listen(3050);
+
+app.listen(porta);
