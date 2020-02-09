118cc46ee7c120d92d3ed00ae28a49492ecf2c73
data52845 <luizes_@hotmail.com>
2020-02-08 21:20:33 -0300

    teste

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const cors = require('cors')
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 //  porta do heroku 
-var porta = process.env.PORT || 8080;
+var porta = process.env.PORT || 5000;
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+
 
 // iniciando o App
 const app = express();
@@ -11,13 +17,22 @@ app.use(express.json());
 app.use(cors());
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodepai', { useNewUrlParser: true }
+//mongoose.connect('mongodb://localhost:27017/nodepai', { useNewUrlParser: true });
 
-);
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+
 requireDir('./src/models');
 
 const Product = mongoose.model('Product');
 
 app.use('/api', require('./src/routes'));
 
-app.listen(porta);
+//app.listen(porta);
