86868cbeaa9cf2b73632e6f43f809d74bbaa20b8
data52845 <luizes_@hotmail.com>
2020-02-10 01:12:27 -0300

    teste mongose

--- server.js
+++ server.js
@@ -2,22 +2,30 @@ const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+var http = require ('http');	 
 
 
-var uristring =
-    process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
-    'mongodb://localhost/HelloMongoose';
+var uristring = 
+  process.env.MONGODB_URI || 
+  'mongodb://localhost/HelloMongoose';
 
-    var theport = process.env.PORT || 5000;
+  var theport = process.env.PORT || 5000;
 
-    mongoose.connect(uristring, function (err, res) {
-      if (err) {
+  mongoose.connect(uristring, function (err, res) {
+    if (err) { 
       console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-      } else {
+    } else {
       console.log ('Succeeded connected to: ' + uristring);
-      }
-    });
+    }
+  });
+
+  var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
+
+
+    http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      createWebpage(req, res);
+    }).listen(theport);
 
 
 
